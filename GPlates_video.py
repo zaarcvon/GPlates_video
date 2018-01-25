5b575f257635e71da561968d18062d9a5ece0023
zaarcvon <34241342+zaarcvon@users.noreply.github.com>
2018-01-25 19:29:59 +0300

    add functionality of making chart on a left side

--- GPlates_video.py
+++ GPlates_video.py
@@ -1,4 +1,4 @@
-def reconstruction_to_video(images_folder='images',output_video='GPlates_reconstruction.avi',fps=15, descending=True):
+def reconstruction_to_video(images_folder='images',output_video='GPlates_reconstruction.avi',fps=15, descending=True,chart=True ,chart_width=150):
     """
     Make video from set of reconstruction images
 
@@ -12,6 +12,10 @@ def reconstruction_to_video(images_folder='images',output_video='GPlates_reconst
         Frame per seconds 
     descending - boolean, default True
         Order of images. True - from older to younger ages.
+    chart - boolean, default True
+        Make a chart with a running line on a left side of a frames
+    chart_width - int, default 150
+        Width of a chart
 
     Returns
     -------
@@ -23,6 +27,7 @@ def reconstruction_to_video(images_folder='images',output_video='GPlates_reconst
     import os
     import cv2
     import re
+    import numpy
     
     images = [img for img in os.listdir(images_folder)]
     
@@ -36,21 +41,39 @@ def reconstruction_to_video(images_folder='images',output_video='GPlates_reconst
     # presettings of video
     frame = cv2.imread(os.path.join(images_folder, images[0]))
     height, width, layers = frame.shape
+    
+        # if chart file doesn't exist in a main directory
+    if os.path.isfile('chart.jpg')==False: 
+        chart=False
+        
+        #get chart and set size as in a frame        
+    if chart==True: 
+        width+=chart_width
+        scale = cv2.imread('chart.jpg')
+        scale = cv2.resize(scale,(chart_width,height))
+    
     video = cv2.VideoWriter(output_video, -1, fps, (width,height))
     
     # descending=True means from older to younger ages
     if descending==True:
         images.sort(key=lambda x: -int(re.findall('(\d+)',x)[0]))
     else: images.sort(key=lambda x: int(re.findall('(\d+)',x)[0]))
+       
     
-    #make video from set of image
-    
+    # main process    
     for img in images:
-        frame = cv2.imread(os.path.join(images_folder, img))
-        # add text on a frame
+        age=re.findall('(\d+)', img)[0]   # get age from filename
+        frame = cv2.imread(os.path.join(images_folder, img)) 
         
-        age=re.findall('(\d+)', img)[0]+' Ma'
-        cv2.putText(frame,age, 
+        # make a chart with line on a frame
+        if chart==True:
+            scale_line=scale.copy()
+            line_position=int(height/150*int(age))
+            cv2.line(scale_line,(0,line_position),(chart_width,line_position),(0,255,0),5)
+            frame = numpy.concatenate((scale_line, frame), axis=1)
+        
+        #  add text on a frame
+        cv2.putText(frame,age+' Ma', 
             bottomLeftCornerOfText, 
             font, 
             fontScale,
